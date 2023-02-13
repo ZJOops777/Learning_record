1f79b7a0886de85ebf61ac7b83297dc581ad846e
ZJOops777 <104690686+ZJOops777@users.noreply.github.com>
2023-02-13 18:38:25 +0800

    practice

--- 2023 2 13 practice/2023 2 13 practice/源.cpp
+++ 2023 2 13 practice/2023 2 13 practice/源.cpp
@@ -66,12 +66,94 @@
 //	return 0;
 //}
 
-int main()
-{
-	int age =10;
-	if(age<18)
-		printf("ok\n");
-	else if (age>=18 && age<26)//if只能管一行printf，多行需要加{}
-		printf("ye ok\n");//else就近原则
-	return 0;
-}
+//int main()
+//{
+//	int age =10;
+//	if(age<18)
+//		printf("ok\n");
+//	else if (age>=18 && age<26)//if只能管一行printf，多行需要加{}
+//		printf("ye ok\n");//else就近原则
+//	return 0;
+//}
+
+//int main()
+//{
+//	int num=11;
+//	if(1==num%2)
+//		printf("jishu\n");
+//	return 0;
+//}
+
+//int main()
+//{
+//	int x=0;
+//	printf("请输入一个整数");
+//	scanf("%d",&x);
+//	if(x%2 == 0) 
+//		printf("%d是偶数\n",x);
+//	else 
+//		printf("%d是奇数\n",x);
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int day=0;
+//	scanf_s("%d",&day);
+//	switch(day)  //整型表达式
+//	{
+//	case 1:    //整型常量表达式
+//		printf("zhou 1");
+//		break;
+//	case 2:
+//		printf("zhou 2");
+//		break;
+//	case 3:
+//		printf("zhou 3");
+//		break;
+//	case 4:
+//		printf("zhou 4");
+//		break;
+//	case 5:
+//		printf("zhou 5");
+//		break;
+//	case 6:
+//		printf("zhou 6");
+//		break;
+//	case 7:
+//		printf("zhou 7");
+//		break;
+//	default:
+//		printf("shu ru cuo wu");
+//		break;
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a=1;
+//	while(a<=10)
+//	{
+//		if (5==a)
+//			break; //while循环中，break用于永久的终止循环
+//		           //continue的作用是跳出本次循环continue后面的代码，直接去判断部分看是否进行下一次循环
+//		printf("%d\n",a);
+//		a++;
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a=1;
+//	int sum=0;
+//	while(a<=100)
+//	{
+//		sum=sum+a;
+//		a++;
+//	}
+//	printf("%d\n",sum);
+//	return  0;
+//}
