557df5d40b3f4afef17bf3f8fcf58e35065354e8
ZJOops777 <104690686+ZJOops777@users.noreply.github.com>
2023-02-14 14:26:54 +0800

    2023 2 14 practice

---
+++ 2023 2 14 practice/2023 2 14 practice/源.cpp
@@ -0,0 +1,51 @@
+#include <stdio.h>
+//int main()
+//{
+//	char password[20]={0};
+//	printf("请输入密码：");
+//	scanf("%s",password);
+//	printf("请确认密码(Y/N):");
+//	
+//	int tmp = 0;       //清除缓冲区，要不然会读取\n
+//	while((tmp=getchar())!='\n');
+//	{
+//		;
+//	}
+//	int ch=getchar();
+//	if('Y'==ch)
+//	{
+//		printf("确认成功\n");
+//	}
+//	else
+//	{
+//		printf("输入错误\n");
+//	}
+//	return 0;
+//}
+
+//int main()
+//{
+//	int i=0;
+//	for (i=1;i<=10;i++)  //初始化；判断；调整
+//	{
+//		if(5==i)
+//		break;   //continue;
+//
+//		printf("%d",i);
+//	}
+//	return 0;
+//}
+
+//int main()  //n的阶乘
+//{
+//	int n = 0;
+//	int i = 0;
+//	int ret =1;
+//	scanf_s("%d",&n);
+//	for(i=1;i<=n;i++)
+//	{
+//		i=ret*i;
+//	}
+//	printf("%d\n",ret);
+//	return 0;
+//}
