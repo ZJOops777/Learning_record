e7cf95a14746a3892bd6b86fba3a29517f514a4e
ZJOops777 <104690686+ZJOops777@users.noreply.github.com>
2023-03-02 13:02:11 +0800

    2023 3 1

---
+++ 2023 3 1 Project1/2023 3 1 Project1/源.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+	char ch1='A',ch2='a';
+	printf("%c\n",(ch1,ch2));
+	return 0 ;
+}
+
---
+++ 2023220practice/2023220practice/源.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+//
+//int main()
+//{
+//	int i;
+//	for(i=0;i<3;i++)
+//	switch (i)
+//	{
+//	case 0:printf("%d",i);
+//	case 2:printf("%d",i);
+//	default:printf("%d",i);
+//
+//	}
+//	return 0;
+//}
