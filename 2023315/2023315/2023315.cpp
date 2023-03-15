eff8fc4e45ec78a9dd77044effe97c175b84e6b5
ZJOops777 <104690686+ZJOops777@users.noreply.github.com>
2023-03-15 10:38:44 +0800

    2023 315

---
+++ 2023315/2023315/2023315.cpp
@@ -0,0 +1,47 @@
+#include <stdio.h>
+//int main()
+//{
+//	int a;
+//	int sum;
+//	for(a=100;a<=200;a++){
+//		if(a%2==0){
+//			sum=a;
+//		}
+//		else{
+//			;
+//		}
+//		sum+=sum;
+//	}
+//	printf("%d",sum);
+//	getchar();
+//	return 0;
+//}
+
+//int get_max(int x, int y) 
+//{
+//	if (x > y) 
+//	{
+//		return x;//若最大值为x则返回x
+//	}
+//	else return y;//否则返回y
+//}
+//int main() 
+//{
+//	int a, b;
+//	printf("请输入a，b的值，a与b之间用逗号隔开\n");
+//	scanf("%d,%d",&a,&b);
+//	int max = get_max(a, b);//对函数的调用
+//	printf("最大值为：%d",max);
+//}
+
+
+int main()
+{
+	int m;
+	printf("请输入学生数量：");
+	scanf_s("%d",m);
+	float 
+	float avg;
+	printf(
+	return 0;
+}
