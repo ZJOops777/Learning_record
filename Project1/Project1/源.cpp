69f370f132dddfe088ed3e6c8c8e30409dbf3901
ZJOops777 <104690686+ZJOops777@users.noreply.github.com>
2023-04-07 15:42:56 +0800

    4.6

--- Project1/Project1/源.cpp
+++ Project1/Project1/源.cpp
@@ -1,19 +1,68 @@
 #include <stdio.h>
+#include <string.h>
 int main()
 {
-	char c1='C';
-	char c2='h';
-	char c3='i';
-	char c4='n';
-	char c5='a';
+	//char c1='C';
+	//char c2='h';
+	//char c3='i';
+	//char c4='n';
+	//char c5='a';
 
-	c1+=5;
-	c2+=5;
-	c3+=5;
-	c4+=5;
-	c5+=5;
+	//c1+=5;
+	//c2+=5;
+	//c3+=5;
+	//c4+=5;
+	//c5+=5;
 
-	printf("%c%c%c%c%c",c1,c2,c3,c4,c5);
+	//printf("%c%c%c%c%c",c1,c2,c3,c4,c5);
+	/*int k1=10,k2=20;
+	(k1=k1>k2)&&(k2=k2>k1);
+	printf("%d,%d",k1,k2);*/
+	/*char str[15]="hello!";*/
+	//int a=2&&3;
+	//printf("%d\n",a);
+
+	/*int x=0;
+	for(x=0;x<100;x++){
+		if(x%3==0){
+			printf("%d\n",x);}
+		else if(x%7==0){
+			printf("%d\n",x);}
+		else ;}*/
+
+	//int k=10;
+	//while(k==0)
+	//	k=k-1;
+	//int i;
+	//int sum=0;
+	//for(i=1;i<=101;i=i+2){
+	//	sum=sum+i;
+	//}
+
+	//int i,j,n1;
+	//char a[20];
+	//gets(a);
+	//n1=strlen(a);
+	//for(i=0;i<n1;i++){
+	//	if((a[i]>='0'&&a[i]<='9')==0)
+	//		printf("%c",a[i]);
+	//}
+
+	//int a;
+	//int	i=0;
+	//int n=0;                                                  
+	//a=82;
+	//for(i=0;i<=5;i++)
+	//{
+	//	if(a/10!=0){
+	//		n+=1;}
+
+
+	//}
+
+int a=2;
+int c=5;
+	printf("a=%%d,b=%%d\n",a,c);
 	getchar();
 	return 0;
 }
--- Project2/Project2/源.cpp
+++ Project2/Project2/源.cpp
@@ -20,9 +20,17 @@ int main()
 {
 //	int a=!!5;
 //	int b=5;
-	int i;
-	for(i=1;i<=5;++i)
-	printf("%d",i);
+	//int i;
+	//for(i=1;i<=5;++i)
+	//int x;
+	//int y;
+	//int i=1;
+	//y=x+1,++x,x+2;
+	//printf("%d",y);
+	int n=0;
+	while(n++<=2)
+		;
+	printf("%d",n);
 	getchar();
 	return 0;
 }
