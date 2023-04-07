971bd74e7e25c8b0886e98334fbb39baacf0dd2b
ZJOops777 <104690686+ZJOops777@users.noreply.github.com>
2023-04-07 16:37:19 +0800

    4.7

---
+++ Project4/Project4/源.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main(){
+	//int i,j,k; //百钱百鸡
+	//int a;
+	//for(i=0;i<=20;i++){
+	//	for(j=0;j<=33;j++){
+	//		k=100-i-j;
+	//			if((5*i+3*j+k/3==100)&&(k%3==0)){
+	//				printf("%d,%d,%d",i,j,k);}}}
+
+	//int i,j,k;   //完全数
+	//for(i=2;i<=10000;i++){
+	//	k=1;
+	//	for(j=2;j<i;j++)
+	//		if(i%j==0)
+	//			k=j+k;
+	//	if(i==k)
+	//		printf("%d ",k);}
+
+
+getchar();
+	return 0;
+}
